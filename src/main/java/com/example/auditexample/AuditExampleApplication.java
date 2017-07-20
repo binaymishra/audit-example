12739342e5eb732959b04bd875093aba19095f90
binaymishramca@gmail.com <binaymishramca@gmail.com>
2017-07-20 21:26:11 +0530

    Initial commit

---
+++ src/main/java/com/example/auditexample/AuditExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.auditexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/auditexample/AuditExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.auditexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuditExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
